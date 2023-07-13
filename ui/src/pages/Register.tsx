9feb2049e8950277678a7f6c4a10d6ce3fe15032
Hikmet Kütük <hikmetkutuk@gmail.com>
2023-07-13 16:47:38 +0300

    add register password validate message

--- ui/src/pages/Register.tsx
+++ ui/src/pages/Register.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import CSS from "csstype";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const initialState = {
@@ -8,11 +9,70 @@ const initialState = {
   confirmPassword: "",
 };
 
+const timesIconStyle: CSS.Properties = {
+  color: "red",
+  fontSize: "17px",
+};
+
+const checkIconStyle: CSS.Properties = {
+  color: "green",
+  fontSize: "17px",
+};
+
 function Register() {
   const [formData, setFormData] = useState(initialState);
   const { name, email, password, confirmPassword } = formData;
 
-  const handleInputChange = () => {};
+  const [uCase, setUCase] = useState(false);
+  const [num, setNum] = useState(false);
+  const [specChar, setSpecChar] = useState(false);
+  const [passLength, setPassLength] = useState(false);
+
+  const checkIcon = <i style={checkIconStyle} className="ni ni-check-bold" />;
+  const timesIcon = <i style={timesIconStyle} className="ni ni-fat-remove" />;
+
+  const changeIcon = (condition) => {
+    if (condition) {
+      return checkIcon;
+    } else {
+      return timesIcon;
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  useEffect(() => {
+    // check lowercase and uppercase
+    if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) {
+      setUCase(true);
+    } else {
+      setUCase(false);
+    }
+
+    // check number
+    if (password.match(/([0-9])/)) {
+      setNum(true);
+    } else {
+      setNum(false);
+    }
+
+    // check special character
+    if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) {
+      setSpecChar(true);
+    } else {
+      setSpecChar(false);
+    }
+
+    // check length
+    if (password.length > 7) {
+      setPassLength(true);
+    } else {
+      setPassLength(false);
+    }
+  }, [password]);
 
   return (
     <div className="container mt--8 pb-5">
@@ -101,8 +161,30 @@ function Register() {
                 </div>
                 <div className="text-muted font-italic">
                   <small>
-                    password strength:{" "}
-                    <span className="text-success font-weight-700">strong</span>
+                    Lowercase & Uppercase:
+                    <span className="font-weight-700">{changeIcon(uCase)}</span>
+                  </small>
+                </div>
+                <div className="text-muted font-italic">
+                  <small>
+                    Number (0-9):
+                    <span className="font-weight-700">{changeIcon(num)}</span>
+                  </small>
+                </div>
+                <div className="text-muted font-italic">
+                  <small>
+                    Special Character (!@#$%&^*+):
+                    <span className="font-weight-700">
+                      {changeIcon(specChar)}
+                    </span>
+                  </small>
+                </div>
+                <div className="text-muted font-italic">
+                  <small>
+                    At least 8 character:
+                    <span className="font-weight-700">
+                      {changeIcon(passLength)}
+                    </span>
                   </small>
                 </div>
                 <div className="text-center">
